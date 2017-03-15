640a2c02de378913eb35429fb6ad3438468e1af4
rybayramov <rybayramov@gmail.com>
2017-03-16 00:52:57 +0400

    alniz log fillardan 7 gun evvel olanlari silir

---
+++ DeleteLogs/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteLogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo source = new DirectoryInfo(System.IO.Directory.GetCurrentDirectory());
+
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                if (fi.Extension == ".log")
+                {
+                    var creationTime = fi.Name.Substring(0, 8);
+
+                    if (DateTime.ParseExact(creationTime,
+                                      "yyyyMMdd",
+                                       CultureInfo.InvariantCulture) < (DateTime.Now - new TimeSpan(7, 0, 0, 0)))
+                    {
+                        fi.Delete();
+                    }
+                }
+            }
+        }
+    }
+}
